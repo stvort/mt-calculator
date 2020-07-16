00b0dd3584ac35d5f12da6343026a4af6ef732e9
unknown <stvort@mail.ru>
2020-07-16 16:36:30 +0400

    add ui

--- mt-calculator-ui/src/store/store.js
+++ mt-calculator-ui/src/store/store.js
@@ -5,61 +5,11 @@ import router from '../router'
 
 Vue.use(Vuex)
 
+const urlAuth = 'https://91i.ru/api/v1/token';
 const urlDashboardAll = 'https://mt-calculator-dashboard.herokuapp.com/api/v1/dashboard/all'
 const urlDashboardSelf = 'https://mt-calculator-dashboard.herokuapp.com/api/v1/dashboard/self'
 const urlTrainingStart = 'https://91i.ru/api/v1/start'
-////const urlTrainingEnd = 'http://localhost:8080/api/v1/end'
 const urlTrainingEnd = 'https://91i.ru/api/v1/end'
-//const urlDashboardSelf = 'http://localhost:8080/api/v1/dashboard/self'
-
-
-/*
-{
-    "session_id": "d78ba523-c6c7-481b-baf3-df3d81e0f8a1",
-    "user_id": 2,
-    "background": "https://picsum.photos/1000/400",
-    "equations": [
-        {
-            "id": 33465,
-            "first_number": 8,
-            "second_number": 11,
-            "type": "m"
-        }
-    }
-}    
-
-
-https://91i.ru/api/v1/end
-
-// Пост
-{
-  "session_id": "39958f17-80b3-47d5-9ecd-9bac65cb47c6",
-  "equation_results": [
-    {
-      "id": 33168,
-      "result": 40
-    }
-  ]
-}
-
-// Ответ
-{
-  "session_id": "39958f17-80b3-47d5-9ecd-9bac65cb47c6",
-  "client_time": 24,
-  "client_score": 3,
-  "client_result_answers": [
-    {
-      "id": 33168,
-      "result": 40,
-      "success": true
-    }
-}    
-
-*/
-
-//const sessionInfoStub = {"session_id":"7a4f5771-772f-4d25-8a59-50fc42442d0f","user_id":2,"background":"https://picsum.photos/1000/400","equations":[{"id":32771,"first_number":1,"second_number":3,"type":"m"},{"id":43452,"first_number":11,"second_number":1,"type":"d"},{"id":33073,"first_number":4,"second_number":11,"type":"m"},{"id":33071,"first_number":4,"second_number":9,"type":"m"},{"id":42766,"first_number":4,"second_number":1,"type":"d"},{"id":42963,"first_number":6,"second_number":2,"type":"d"},{"id":33659,"first_number":10,"second_number":8,"type":"m"},{"id":43060,"first_number":7,"second_number":1,"type":"d"},{"id":33266,"first_number":6,"second_number":8,"type":"m"},{"id":32974,"first_number":3,"second_number":10,"type":"m"}]}
-
-
 
 export default new Vuex.Store({
     state: {
@@ -71,7 +21,7 @@ export default new Vuex.Store({
         currentLogin: '',
         commonResults: [],
         selfResults: [],
-        trainingSessionInfo: undefined//sessionInfoStub,
+        trainingSessionInfo: undefined,
     },
     getters: {
         getIsLoggedIn (store) {
@@ -157,7 +107,7 @@ export default new Vuex.Store({
 
             state.currentLogin = undefined;
             state.isLogging=true
-            requestToken('https://91i.ru/api/v1/token', loginInfo).then(tokenData => {
+            requestToken(urlAuth, loginInfo).then(tokenData => {
                 state.isLogging = false
                 state.isLoggedIn = true
                 state.currentLogin = loginInfo.loginText;
